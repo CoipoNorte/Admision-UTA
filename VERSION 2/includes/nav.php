b2d7077a002ca6e54f99713d72e94e934d9ec310
CoipoNorte <christiancaceres1398@gmail.com>
2023-12-19 20:51:04 -0300

    .

--- VERSION 2/includes/nav.php
+++ VERSION 2/includes/nav.php
@@ -1,9 +1,8 @@
 <!--NAVAR-->
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse shadow">
-    <div class="position-sticky mt-4">
-
+    <div id="navcito" class="position-sticky mt-3">
         <ul class="nav flex-column">
-        <?php include_once '../util/listar_botones.php'; ?>
+            <?php include_once '../util/listar_botones.php'; ?>
         </ul>
     </div>
 </nav>
